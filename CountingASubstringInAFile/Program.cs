4f154414d587fb5d5be56d887224e7d385fde6ea
Daniel <64807656+DanielNinchev@users.noreply.github.com>
2020-11-22 00:57:43 +0200

    Add files via upload

---
+++ CountingASubstringInAFile/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace CountingASubstringInAFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Да се преброи колко пъти се среща поднизът "С#" в текстов файл.
+
+            string fileName = @"..\..\sample.txt";
+            string word = "C#";
+
+            try
+            {
+                StreamReader reader = new StreamReader(fileName);
+
+                using (reader)
+                {
+                    int occurrences = 0;
+                    string line = reader.ReadLine();
+
+                    while (line != null)
+                    {
+                        int index = line.IndexOf(word);
+
+                        while (index != -1)
+                        {
+                            occurrences++;
+                            index = line.IndexOf(word, (index + 1));
+                        }
+
+                        line = reader.ReadLine();
+                    }
+
+                    Console.WriteLine($"The word {word} occurs {occurrences} times in the file.");
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.Error.WriteLine("Cannot find file {0}.", fileName);
+            }
+            catch(IOException)
+            {
+                Console.Error.WriteLine("Cannot read the file {0}.", fileName);
+            }
+        }
+    }
+}
---
+++ DeleteOddLinesFromFile/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace DeleteOddLinesFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "names.txt";
+
+            string[] lines = File.ReadAllLines(file);
+
+            int counter = 0;
+
+            try
+            {
+                StreamWriter writer = new StreamWriter(file, false);
+
+                using (writer)
+                {
+                    foreach (var line in lines)
+                    {
+                        counter++;
+
+                        if (counter % 2 == 0)
+                        {
+                            writer.WriteLine(line);
+                        }
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.Error.WriteLine($"The file {file} could not be found.");
+            }
+            catch (IOException)
+            {
+                Console.Error.WriteLine($"The file {file} could not be read.");
+            }
+        }
+    }
+}
